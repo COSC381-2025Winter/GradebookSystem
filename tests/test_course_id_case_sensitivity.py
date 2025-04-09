aedf0ba55035c76f43ee8c3e9867c15f23159ae9
Bear Kennedy <196842211+BearKennedy@users.noreply.github.com>
2025-04-09 23:37:09 +0000

    removed extra empty lines

--- tests/test_course_id_case_sensitivity.py
+++ tests/test_course_id_case_sensitivity.py
@@ -1,9 +1,5 @@
 from main import main
 import pytest, test_main
-
-
-    
-   
     
 @pytest.fixture
 def expected_output():
@@ -13,10 +9,8 @@ def expected_output():
 3. view grades
 4. logout
 """
-
     return x
-
-    
+  
 def test_Course_selection_with_lowercase(monkeypatch, capsys,expected_output):
 # Act & Arrange
     responses = iter(["101","cs101", '4', ' ', 'q'])
@@ -58,5 +52,3 @@ def test_Course_selection_with_mixed_input(monkeypatch, capsys,expected_output):
    
   # Assert
     assert s in captured.out.lower()
-
-    

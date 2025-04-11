73ad5ad98f042b5278f33795b8ba6ae9f698f994
Caleb Bujauskas <196842407+CalebBujauskas@users.noreply.github.com>
2025-04-10 22:37:41 -0400

    Updated caleb_test_search_student.py & gradebook.py
    - Added two more tests including test if search
      query of a single letter will display all students
      names that contain that letter. As well as test
      if message displayed when no matches found.
    - Added message to be displayed when no matches
      found in search_student function.
    
    issue #51

--- tests/caleb_test_search_student.py
+++ tests/caleb_test_search_student.py
@@ -31,14 +31,14 @@ def test_student():
     # uses a student from data.py
     # 201: "Alice"
     return {
-        "id": 201,
-        "name": "Alice"
+        "id": [201, 203],
+        "name": ["Alice", "Charlie"]
     }
 
 # test if matching student is displayed after searching for one specific student using their id
 def test_matching_student_displayed(monkeypatch, capsys, test_instructor, test_student, test_course):
     # Arrange
-    responses = iter([test_instructor["id"], test_course["id"], "4", str(test_student["id"]), "\n", "back", "5", "\n", "q"])
+    responses = iter([test_instructor["id"], test_course["id"], "4", str(test_student["id"][0]), "\n", "back", "5", "\n", "q"])
     monkeypatch.setattr('builtins.input', lambda _: next(responses))
 
     # Act
@@ -47,4 +47,36 @@ def test_matching_student_displayed(monkeypatch, capsys, test_instructor, test_s
 
     # Assert
     captured = capsys.readouterr()
-    assert test_student["name"] in captured.out
+    assert test_student["name"][0] in captured.out
+    assert str(test_student["id"][0]) in captured.out
+
+# test if matching students are displayed after searching for letter "c" that may be apart of multiple students; names
+def test_display_students_containing_letter_match(monkeypatch, capsys, test_instructor, test_student, test_course):
+    # Arrange
+    responses = iter([test_instructor["id"], test_course["id"], "4", "c", "\n", "back", "5", "\n", "q"])
+    monkeypatch.setattr('builtins.input', lambda _: next(responses))
+
+    # Act
+    with pytest.raises(SystemExit) as exitInfo:
+        main()
+
+    # Assert
+    captured = capsys.readouterr()
+    assert test_student["name"][0] in captured.out
+    assert str(test_student["id"][0]) in captured.out
+    assert test_student["name"][1] in captured.out
+    assert str(test_student["id"][1]) in captured.out
+
+# test if no matching students found displays message
+def test_no_matching_students(monkeypatch, capsys, test_instructor, test_student, test_course):
+    # Arrange
+    responses = iter([test_instructor["id"], test_course["id"], "4", "zxy", "\n", "back", "5", "\n", "q"])
+    monkeypatch.setattr('builtins.input', lambda _: next(responses))
+
+    # Act
+    with pytest.raises(SystemExit) as exitInfo:
+        main()
+
+    # Assert
+    captured = capsys.readouterr()
+    assert "No matching students found." in captured.out
